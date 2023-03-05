a21b72cfd4a5fbaad69ce17775aad0d49a42f8cd
seongyeon12 <k.seongyeon12@gmail.com>
2023-03-05 14:51:21 +0900

    Add path

--- the_wisegroup/src/App.tsx
+++ the_wisegroup/src/App.tsx
@@ -1,6 +1,7 @@
 import MainView from './component/pages/main/MainView';
+import TmpView from '@pages/tmp/TmpView';
+import '@css/index.css'
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 
 // 라우터 - 연결
 
@@ -8,6 +9,7 @@ const App = (): JSX.Element => {
   return (
       <Routes>
           <Route path='/' element={<MainView />}/>
+          <Route path='/tmp' element={<TmpView />}/>
       </Routes>
   )
 }
