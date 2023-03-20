ae4981eb48b228a1ec53c15b0043f88d43822fec
seongyeon12 <k.seongyeon12@gmail.com>
2023-03-20 23:14:25 +0900

    Add footer

---
+++ the_wisegroup/src/pages/footer/FooterView.tsx
@@ -0,0 +1,11 @@
+import styles from '@css/footer.module.css' 
+
+const FooterView = (): JSX.Element => {
+  return (
+    <footer className={styles.footer}>
+      푸터내용
+    </footer>
+  )
+}
+
+export default FooterView
